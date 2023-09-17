a412aaac104c93ea306df986a4350272d72ae76b
Andres Patiño <97bermduez.andres@gmail.com>
2023-09-16 21:38:53 -0500

    nuevos ajustes

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,1 +1,131 @@
+$(document).ready(function () {
+  $(".banner__items").slick({
+    dots: true,
+    infinite: false,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  });
 
+  $(".program__items").slick({
+    dots: true,
+    infinite: false,
+    speed: 1000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+    ],
+  });
+
+  $(".blog__items").slick({
+    dots: true,
+    infinite: false,
+    speed: 1500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+    ],
+  });
+
+  $(".podcast__items").slick({
+    dots: true,
+    infinite: false,
+    speed: 2000,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+    ],
+  });
+
+  $(".team__items").slick({
+    dots: true,
+    infinite: false,
+    speed: 1500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+    ],
+  });
+
+  $(".other_programs__slide").slick({
+    dots: true,
+    infinite: false,
+    speed: 1500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [],
+  });
+});
